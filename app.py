1ce76335ad7a35cf886e9aa0202d6ce0f0ed2123
Ran Xiao <ran.xiao@auspost.com.au>
2018-04-17 16:26:22 +1000

    test metaphone is working

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from chalice import Chalice
 import json
-#import demo
+from metaphone import DoubleMetaphone
 
 app = Chalice(app_name='storm-name-matching-ec2')
 
 
 @app.route('/match/{name}')
 def index(name):
-    return {"name":name}
+    return {"name":name, "key": DoubleMetaphone().parse(name)}
